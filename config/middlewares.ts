21d4a0019fdaba656065930a450aedc43d3035b8
AhmedBokaniUsman <ahmedbokaniusman5@gmail.com>
2025-04-28 16:46:37 +0100

    Initial commit

---
+++ config/middlewares.ts
@@ -0,0 +1,35 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http://localhost:3000'], // Allow React app
+          'img-src': ["'self'", 'http://localhost:1337'], // Allow image URLs
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
